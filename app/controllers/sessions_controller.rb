fb639257fad7e50eb54d44a17348c05fd08ca6d9
Jerry Pallath <jpallath@nyu.edu>
2015-07-20 13:11:44 -0400

    doo doo doo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,23 +9,21 @@ class SessionsController < ApplicationController
   # end
   def create
     auth_hash = request.env['omniauth.auth']
-    puts(auth_hash)
     @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-
-    session[:user_id] = "Salim"
+    session[:user_id] = auth_hash[:uid]
     if @authorization
       redirect_to "/games/show"
     else
-      user = User.new :name => auth_hash["info"]["name"], :email => auth_hash["info"]["email"]
+      user = User.new :name => auth_hash["info"]["name"], :uid => auth_hash[:uid]
       user.save
-      redirect_to "/users/new"
+      redirect_to "/games"
     end
   end
 
 
   def destroy
     session[:user_id] = nil
-    redirect_to "/users/new"
+    redirect_to "/games/new"
   end
 
 
