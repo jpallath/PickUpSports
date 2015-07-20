8d7e06aa97b9af5ae2af8f9f91abdca666613472
Jerry Pallath <jpallath@nyu.edu>
2015-07-20 13:46:48 -0400

    final merge

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,5 +6,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['DEVISE_FB_API'], ENV['DEVISE_FB_SECRET']
+  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret']
 end
