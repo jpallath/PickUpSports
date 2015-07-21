d1ce33349391b53c8a11379cdac5aa29f2b79a4e
Jerry Pallath <jpallath@nyu.edu>
2015-07-21 02:13:08 -0400

    deleted stuff

--- app/models/game.rb
+++
@@ -1,5 +0,0 @@
-class Game < ActiveRecord::Base
-  belongs_to :user, class_name: "creator", foreign_key: :user_id
-  has_many :enrollments
-  has_many :players, through: :enrollments
-end
--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :created_games, class_name: "Game", foreign_key: :user_id
-  has_many :enrollments
-  has_many :enrolled_games, through: :enrollments, source: :game
-end
